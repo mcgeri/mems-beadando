00e5096c1dd2a15cb8f2bd780e15d04af2337e74
mcgeri <43815333+mcgeri@users.noreply.github.com>
2018-10-04 15:32:19 +0200

    Add files via upload

--- mems-beadando.ino
+++ mems-beadando.ino
@@ -1,13 +1,8 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
+// DHT humidity/temperature sensors
+
 
 #include "DHT.h"
-
 #define DHTPIN 2     // what digital pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 #define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
@@ -25,7 +20,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
+  Serial.println("DHT21 test!");
 
   dht.begin();
 }
